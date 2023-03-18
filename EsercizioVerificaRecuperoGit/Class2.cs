2714d6ce091fe3e7a90f59d0ad7e1e58c259c9f1
Checchox <francesco.serrani.stud@ispascalcomandini.it>
2023-03-18 08:36:18 +0100

    Classe Alunno1

---
+++ EsercizioVerificaRecuperoGit/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioVerificaRecuperoGit
+{
+    internal class Class2
+    {
+    }
+}
